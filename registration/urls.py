27d02bf759da6b3f1fc4ba079b9a23da33d1a360
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-07 12:00:21 +0545

    dajbgo start

--- registration/urls.py
+++ registration/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [path('',views.index,name='index'),
                path('about',views.about,name='about'),
                path('login',views.login,name='login'),
                path('register',views.register,name='register'),
+               path('signup',views.signup,name='signup'),
                path('blogs',views.blogs,name='blogs'),
                ]
--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
@@ -18,5 +18,19 @@ def login(request):
 
 def blogs(request):
     return render(request,'blogs.html')
-# def signup(request):
-#     if(request.method == 'POST'):
+
+def signup(request):
+    error_messsage = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+    if password!=cpassword:
+        error_messsage="Password doesnot match"
+    else:
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        return redirect('login')
+    
+    return render(request,'register.html',{"error_messsage":error_messsage})
